ac0054472902b83756a00d15437570d294199c39
Gabriel Hewitt <97916103+ghewitt95@users.noreply.github.com>
2025-03-02 23:59:00 +0000

    finished practice

--- app.rb
+++ app.rb
@@ -1,18 +1,55 @@
 require "sinatra"
 require "sinatra/reloader"
 
-get("/") do
+get ("/") do
   erb (:home)
 end
 
 get("/rock") do
+  moves = ["rock", "paper", "scissors"]
+
+  @comp_move = moves.sample
+
+  if @comp_move == "rock"
+    @outcome = "tied"
+  elsif @comp_move == "paper"
+    @outcome = "lost"
+  else
+    @outcome = "won"
+
+  end
+
   erb (:rock)
 end
 
-get("/paper") do
+get ("/paper") do
+  moves = ["rock", "paper", "scissors"]
+
+  @comp_move = moves.sample
+
+  if @comp_move == "rock"
+    @outcome = "won"
+  elsif @comp_move == "paper"
+    @outcome = "tied"
+  else
+    @outcome = "lost"
+  end
+  
   erb (:paper)
 end
 
-get("/scissors") do
+get ("/scissors") do
+  moves = ["rock", "paper", "scissors"]
+
+  @comp_move = moves.sample
+
+  if @comp_move == "rock"
+    @outcome = "lost"
+  elsif @comp_move == "paper"
+    @outcome = "won"
+  else
+    @outcome = "tied"
+  end
+  
   erb (:scissors)
 end
